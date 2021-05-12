106c1b92030c9b80189abf776401fae5672b7672
Raihan Parama <64841350+raihanpl@users.noreply.github.com>
2021-05-12 23:44:56 +0700

    update profile command

--- basic.py
+++ basic.py
@@ -21,6 +21,7 @@ mongosrv = config.get('auth', 'mongosrv')
 cluster = MongoClient(mongosrv)
 db = cluster['ValorantBot']
 collection = db['agent']
+user_coll = db['user']
 
 intents = discord.Intents.default()
 intents.members = True
@@ -36,18 +37,46 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=5, name="Life"))
 
 @bot.command()
+async def register(ctx):
+    id_user = ctx.message.author.id
+    name_user = ctx.message.author.name
+    if user_coll.count_documents({'_id': id_user }, limit = 1) != 1:
+        post = {
+        "_id" : id_user,
+        "name" : name_user,
+        "points" : 0
+        }
+        user_coll.insert_one(post)
+        await ctx.send("Sucessfully Registered")
+    else :
+        await ctx.send("Already Registered")
+
+@bot.command()
+async def profile(ctx):
+    id_user = ctx.message.author.id
+    data_user = user_coll.find_one({"_id" : id_user})
+    namauser = data_user['name']
+    point = data_user['points']
+    id_user = data_user['_id']
+    
+    author_ava = ctx.message.author.avatar_url
+    embed = makeembeduser(ctx, namauser, author_ava, point)
+    await ctx.send(embed=embed)
+
+@bot.command()
 async def gacha(ctx):
     judul_data = collection.aggregate([{"$sample":{"size":1}}])
+    print(judul_data)
     for result in judul_data :
         judul = result['name']
         ava_url = result['ava_URL']
         rating = result['rating']
         tipe = result['type']
-    embed = makeembed(ctx, judul, ava_url, rating, tipe)
+    embed = makeembedagent(ctx, judul, ava_url, rating, tipe)
 
     await ctx.send(embed=embed)
 
-def makeembed(ctx, judul, ava_url, rating, tipe):
+def makeembedagent(ctx, judul, ava_url, rating, tipe):
     embed = discord.Embed(
         title=judul,
         colour = discord.Colour.blue()
@@ -60,6 +89,24 @@ def makeembed(ctx, judul, ava_url, rating, tipe):
     embed.set_author(name=author, icon_url=author_ava)
     embed.add_field(name='Rating', value=rating, inline=True)
     embed.add_field(name='Type', value=tipe, inline=True)
+    return embed
+
+def makeembeduser(ctx, name, user_url, point):
+    embed = discord.Embed(
+        title=name,
+        colour = discord.Colour.blue()
+    )
+    author = ctx.message.author.name
+    author_ava = ctx.message.author.avatar_url
+
+    embed.set_footer(text="©Valorant BattleBot")
+    embed.set_image(url=user_url)
+    embed.set_author(name=author, icon_url=author_ava)
+    embed.add_field(name='Point', value=point, inline=True)
+    embed.add_field(name='Owned', value="Not Found", inline=False)
+    embed.add_field(name='Cooldown 1', value="Not Found", inline=True)
+    embed.add_field(name='Cooldown 2', value="Not Found", inline=True)
+    embed.add_field(name='Cooldown 3', value="Not Found", inline=True)
     return embed
 
 @bot.command()
